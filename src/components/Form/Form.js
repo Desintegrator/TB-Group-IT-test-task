b5a36436021d95f23a482baeaa6906cfc8241f0c
Danila Nikonets <danila.nikonets@gmail.com>
2018-07-29 23:55:45 +0600

    Form layout

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Switch from '../Switch/Switch';
+import Field from '../Field/Field';
+import Slider from '../Slider/Slider';
+import './Form.css';
 
-const Form = () => {
-  return (
-    <div>
+export default class Form extends Component {
+    constructor(props) {
+        super(props);
+        this.state={};
+        this.state.additionalFields=false;
+    }
 
-    </div>
-  );
+    render() {
+        const {
+            title,
+        } = this.props;
+
+        return (
+            <form className="form">
+                <h1 className="form__title">{title}</h1>
+                <Field title="Поле 1">
+                    <Switch />
+                </Field>
+                <Field title="Дополнительные поля">
+                    <Switch onClick={isActive => this.setState({ additionalFields: isActive })} />
+                </Field>
+                {this.state.additionalFields ?
+                    <Field title="Дополнительное поле">
+                        <Switch />
+                    </Field> : null}
+            </form>
+        );
+    }
 };
 
-export default Form;
+Form.propTypes = {
+    title: PropTypes.string.isRequired,
+}
