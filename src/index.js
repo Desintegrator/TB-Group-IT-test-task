9455df7f4e4045b67df0939895dd43c6ef170d4d
Danila Nikonets <danila.nikonets@gmail.com>
2018-07-29 22:06:43 +0600

    Removed extra directory and useless files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Demo from './components/Demo/Demo';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Demo />, document.getElementById('root'));
 registerServiceWorker();
