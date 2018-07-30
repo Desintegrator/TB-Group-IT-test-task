2d6d627f9d2cef14b6cd76be4521866ef4607fce
Danila Nikonets <danila.nikonets@gmail.com>
2018-07-31 00:52:40 +0600

    add slider to page

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,35 +6,44 @@ import Slider from '../Slider/Slider';
 import './Form.css';
 
 export default class Form extends Component {
-    constructor(props) {
-        super(props);
-        this.state={};
-        this.state.additionalFields=false;
-    }
+  constructor(props) {
+    super(props);
+    this.state={};
+    this.state.additionalFields=false;
+    this.state.sliderValue=0;
+  }
 
-    render() {
-        const {
-            title,
-        } = this.props;
+  render() {
+    const {
+      title,
+    } = this.props;
 
-        return (
-            <form className="form">
-                <h1 className="form__title">{title}</h1>
-                <Field title="Поле 1">
-                    <Switch />
-                </Field>
-                <Field title="Дополнительные поля">
-                    <Switch onClick={isActive => this.setState({ additionalFields: isActive })} />
-                </Field>
-                {this.state.additionalFields ?
-                    <Field title="Дополнительное поле">
-                        <Switch />
-                    </Field> : null}
-            </form>
-        );
-    }
+    return (
+      <form className="form">
+        <h1 className="form__title">{title}</h1>
+        <Field title="Поле 1">
+          <Switch />
+        </Field>
+        <Field title="Дополнительные поля">
+          <Switch
+            onClick={isActive => this.setState({ additionalFields: isActive })}
+            onText="ON"
+            offText="OFF"
+          />
+        </Field>
+        {this.state.additionalFields ?
+          <Field title="Поле 3">
+            <Switch />
+          </Field> : null}
+        <Field title="Поле 4">
+          <div className="form__subtitle">Value: {this.state.sliderValue}</div>
+          <Slider onChange={value => this.setState({ sliderValue: value })} />
+        </Field>
+      </form>
+    );
+  }
 };
 
 Form.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 }
